a67d5faf446392d90157e0c0059bafa0268d1f24
Yuya Ochiai <yuya0321@gmail.com>
2018-06-21 23:58:11 +0900

    Correct dmg artifact in CircleCI

--- scripts/cp_artifacts.sh
+++ scripts/cp_artifacts.sh
@@ -10,5 +10,6 @@ cp "${SRC}/mattermost-${VERSION}-win-x64.zip" "${DEST}/mattermost-desktop-${VERS
 cp "${SRC}/squirrel-windows/mattermost-setup-${VERSION}.exe" "${DEST}/mattermost-setup-${VERSION}-x64.exe"
 cp "${SRC}"/squirrel-windows-ia32/*.exe "${DEST}/"
 cp "${SRC}"/mattermost-desktop-*.zip "${DEST}/"
+cp "${SRC}"/*.dmg "${DEST}/"
 cp "${SRC}"/*.tar.gz "${DEST}/"
 cp "${SRC}"/*.deb "${DEST}/"
