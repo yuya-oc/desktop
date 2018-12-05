ac0ffd2b59d5f4a2c6acaf5a7e760974bb4d431e
Yuya Ochiai <yuya0321@gmail.com>
2018-12-05 21:34:50 +0900

    Tweak CircleCI scripts

--- scripts/cp_artifacts.sh
+++ scripts/cp_artifacts.sh
@@ -7,7 +7,7 @@ DEST=$2
 
 cp "${SRC}/mattermost-${VERSION}-win-ia32.zip" "${DEST}/mattermost-desktop-${VERSION}-win32.zip"
 cp "${SRC}/mattermost-${VERSION}-win-x64.zip" "${DEST}/mattermost-desktop-${VERSION}-win64.zip"
-cp "${SRC}/squirrel-windows/mattermost-setup-${VERSION}.exe" "${DEST}/mattermost-setup-${VERSION}-win64.exe"
+cp "${SRC}/squirrel-windows/mattermost-setup-${VERSION}-x64.exe" "${DEST}/mattermost-setup-${VERSION}-win64.exe"
 cp "${SRC}/squirrel-windows-ia32/mattermost-setup-${VERSION}-ia32.exe" "${DEST}/mattermost-setup-${VERSION}-win32.exe"
 cp "${SRC}"/mattermost-desktop-*.zip "${DEST}/"
 cp "${SRC}"/*.tar.gz "${DEST}/"
